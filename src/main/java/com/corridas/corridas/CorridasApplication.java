da9c48194e5f5c58f842adaab82c923b0386e428
Unknown <marcelosleal@outlook.com>
2018-05-12 16:43:43 -0300

    Inicio
    primeiro xpon

---
+++ src/main/java/com/corridas/corridas/CorridasApplication.java
@@ -0,0 +1,12 @@
+package com.corridas.corridas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorridasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorridasApplication.class, args);
+	}
+}
